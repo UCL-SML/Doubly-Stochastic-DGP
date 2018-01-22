3e96455dd2859962ad6c72c8b7f1d522720374ea
hrs13 <hrs13@ic.ac.uk>
2018-01-22 17:34:55 +0000

    added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Doubly-Stochastic-DGP',
+            version='1.0',
+            author="Hugh Salimbeni",
+            author_email="hrs13@ic.ac.uk",
+            license="Apache License 2.0",
+            packages=["doubly_stochastic_dgp"],
+            classifiers=['License :: OSI Approved :: Apache Software License']
+      )
+
